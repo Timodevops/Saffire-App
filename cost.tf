cf6b888b7b29c2bb2f3313a2ae6f7b984a59aad6
timothy ige <timoige87@gmail.com>
2025-07-14 20:35:13 -0600

    Add Dev code

---
+++ cost.tf
@@ -0,0 +1,21 @@
+# AWS Budget to monitor monthly spending for EC2
+resource "aws_budgets_budget" "saffire-dev_budget" {
+  name              = "AppBudget"
+  budget_type       = "COST"
+  limit_amount      = "500"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator = "GREATER_THAN"
+    threshold           = 100.0
+    threshold_type      = "PERCENTAGE"
+    notification_type   = "ACTUAL"
+    # List of subscribers for the notification
+    subscriber_email_addresses = ["timoige87@gmail.com"]
+  }
+
+  tags = {
+    Name = "saffire-dev_budget"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,4 @@
+# Configure AWS Provider with the target region
+provider "aws" {
+  region = "us-west-2" # Specify AWS region for resource deployment
+}
