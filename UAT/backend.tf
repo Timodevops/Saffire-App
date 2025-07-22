f504a21950fb3bcb27d3e6c22876e5bd26029dff
timothy ige <timoige87@gmail.com>
2025-07-21 20:42:55 -0600

    update

--- UAT/backend.tf
+++ UAT/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "s3" {
 
-    bucket         = "saffire-uat-app-buckett"
+    bucket         = "saffire-uat-app-bucket"
     key            = "uat/terraform.tfstate"         # Use a unique path per environment
     region         = "us-west-2"
     #dynamodb_table = "saffire-uat-app-db"
     encrypt        = true
   }
 }
+
